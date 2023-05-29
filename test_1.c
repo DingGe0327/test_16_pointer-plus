b926b6402a68b821d14addfeaacf9988c607a0e4
rare200327 <435897971@qq.com>
2023-05-29 19:18:30 +0800

    二级指针

--- test_1.c
+++ test_1.c
@@ -1,14 +1,22 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>  //P49 
+//#include <stdio.h>  //P49 
+//int main()
+//{
+//	char* ps = "hello word";   //ps存储的是字符串首元素的地址
+//	printf("%c\n", *ps);  //h
+//	printf("%s\n", ps);  //hello word
+//	return 0;
+//}
+
+#include <stdio.h>
 int main()
 {
-	char* ps = "hello word";   //ps存储的是字符串首元素的地址
-	printf("%c\n", *ps);  //h
-	printf("%s\n", ps);  //hello word
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	//int (*parr)[] = arr;
+	int *parr = arr;
+	printf("%d\n", parr[5]);
 	return 0;
 }
-
-
 
 
 
