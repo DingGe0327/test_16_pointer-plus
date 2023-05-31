4efce5a7836aed109a9cb66d48dfbe76ba420b1c
rare200327 <435897971@qq.com>
2023-05-31 14:05:58 +0800

    二级指针

--- test_1.c
+++ test_1.c
@@ -11,13 +11,35 @@
 #include <stdio.h>
 int main()
 {
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	//int (*parr)[] = arr;
-	int *parr = arr;
-	printf("%d\n", parr[5]);
+	//int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	////int (*parr)[] = arr;   //parr是一个指向整数数组的指针
+	//int *parr = arr;         //parr 是一个整数指针
+	//printf("%d\n", parr[5]);
+	double* d[3];
+	double* (*pd) = &d;
 	return 0;
 }
 
+//二级指针
+//#include <stdio.h>
+//
+//void modifyValue(int** pp)
+//{
+//    int newValue = 10;
+//    *pp = &newValue;
+//}
+//
+//int main()
+//{
+//    int value = 5;
+//    int* p = &value;
+//
+//    printf("Before modification: %d\n", *p);
+//    modifyValue(&p);
+//    printf("After modification: %d\n", *p);
+//
+//    return 0;
+//}
 
 
 
