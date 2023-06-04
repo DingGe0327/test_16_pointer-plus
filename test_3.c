30ecdcb23f0500555d27f973ae9611170bc551c0
rare200327 <435897971@qq.com>
2023-06-04 19:55:30 +0800

    整形数组

---
+++ test_3.c
@@ -0,0 +1,14 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//int arr[10];          整形数组
+//int *parr[10];		整形指针的数组
+//int (*parr2)[10];		数组指针，该指针能够指向一个数组，数组十个元素，每个元素的类型是int型
+//int (*parr3[10])[5];  
+
+
+
+
+
+
+
+
+
